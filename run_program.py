04354253ef13f5b231ca1970bf5df11e942b820f
Adrian Szczęch <adrianszcz64@gmail.com>
2024-03-29 17:47:04 +0100

    Transfer all data files into csv files

---
+++ run_program.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import csv
+import json
+import os
+from global_variables import *
+from convert_into_csv import *
+
+customers_csv_processed(CUSTOMERS_CSV_FILEPATH_OLD, CUSTOMERS_CSV_FILEPATH_NEW, SELLERS_CSV_FILEPATH_OLD)
+sellers_csv_processed(SELLERS_CSV_FILEPATH_OLD, SELLERS_CSV_FILEPATH_NEW)
+sales_transactions_csv_processed(SALES_TRANSACTIONS_JSON_FILEPATH_OLD, SALES_TRANSACTIONS_CSV_FILEPATH_NEW, CUSTOMERS_CSV_FILEPATH_OLD, SELLERS_CSV_FILEPATH_OLD)
+products_csv_processed(PRODUCTS_CSV_FILEPATH_OLD, PRODUCTS_CSV_FILEPATH_NEW)
+inventory_status_csv_processed(INVENTORY_STATUS_CSV_FILEPATH_OLD, INVENTORY_STATUS_CSV_FILEPATH_NEW)
